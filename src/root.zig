e20eb4762914fe967dfaf0e4bdb0278e26fe2855
ethantstanger <ethantstanger@gmail.com>
2025-11-16 16:02:19 -0700

    updated window functions

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,23 @@
 const raylib = @cImport(@cInclude("raylib.h"));
 
-pub const window = struct {
-    pub const init = raylib.InitWindow;
-    pub const deinit = raylib.CloseWindow;
-    pub const shouldClose = raylib.WindowShouldClose;
+const WindowError = error{InitError};
+
+pub const Window = struct {
+    pub fn init(title: [*c]const u8, width: u32, height: u32) WindowError!Window {
+        raylib.InitWindow(width, height, title);
+        if (!raylib.IsWindowReady()) return WindowError;
+        return .{};
+    }
+    pub fn deinit(_: *Window) void {
+        raylib.CloseWindow();
+    }
+
+    pub fn shouldClose(_: *Window) void {
+        raylib.WindowShouldClose();
+    }
+
+    pub fn draw(_: *Window) void {
+        raylib.BeginDrawing();
+        defer raylib.EndDrawing();
+    }
 };
