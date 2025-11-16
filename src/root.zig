d4c5d5be4e6b4a05b8056d9cd17f9126e49c454e
ethantstanger <ethantstanger@gmail.com>
2025-11-16 15:05:32 -0700

    lowercased namespace

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,6 @@
 const raylib = @cImport(@cInclude("raylib.h"));
 
-const Window = struct {
+const window = struct {
     const init = raylib.InitWindow;
     const deinit = raylib.CloseWindow;
     const shouldClose = raylib.WindowShouldClose;
