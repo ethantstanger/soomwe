48e9684028fb76515516310eda0247d415a89c9d
ethantstanger <ethantstanger@gmail.com>
2025-11-16 14:46:55 -0700

    added standard optimization options

--- build.zig
+++ build.zig
@@ -2,10 +2,12 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("soomwe", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
     const mod_tests = b.addTest(.{
